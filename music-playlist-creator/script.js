31478841c276ab4d79a4626d998ca56116bf4d4d
Hanna Abrahem <habrahem@haverford.edu>
2024-06-11 19:59:26 -0700

    got a lot of the modal to work

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,62 +1,89 @@
 //////////////////////////// MODAL CODE
 
-// buttons to press to get the modal
-var openingButton = document.querySelector(".playlist-card");
-
-function openModal() {}
-
 // Get the button that opens the modal
-var model = document.getElementById("dropDown");
+var modal = document.getElementsByClassName("modal")[0];
+
+// add the image variable
+var img = document.getElementsByClassName("image-placeholder");
 
 // Get the <span> element that closes the modal
-var span = document.getElementByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-function openModel(playlist) {
-  document.getElementById("playlistName").innerText = playlist.name;
-  document.getElementById("playlistImage").src = festival.imageUrl;
+//function openModal(playlist) {
+//  document.getElementById("sub-playlist").innerText = playlist.name;
+//  document.getElementById("playlistImage").src = playlist.imageUrl;
 
-  // When the user clicks on the button, open the modal
-  modal.style.display = "block";
-}
+// When the user clicks on the button, open the modal
+//  modal.style.display = "block";
+//}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
+  console.log("I closed the function");
   modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
+  console.log("I opened the modal");
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
+
+for (let i = 0; i < img.length; i++) {
+  console.log("the loop is opened");
+  img[i].addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+}
+
+span.addEventListener("click", () => {
+  console.log("the second way to open");
+  modal.style.display = "none";
+});
+
 ///////////////////////////////////////
 
 //// MILESTONE 3: DISPLAYING SHARING PLAYLISTS
 
-const container = document.querySelector(".playlist-cards");
-container.innerHTML = ""; //clear any existing content
-
 function creatingCards(data) {
+  //const container = document.querySelector(".playlist-cards");
+
   for (let i = 0; i < data.playlists.length; i++) {
+    const container = document.querySelector(".playlist-cards");
     // each playlist
     const playlist = data.playlists[i];
 
+    // creating card
+    const card = document.createElement("div");
+    card.className = "playlist-card";
+
     // creating image
-    const image = document.getElementByClassName("image-placeholder");
-    image.textContent += "${playlist.playlist_art}";
+    const image = document.createElement("img");
+    image.className = "image-placeholder";
+    image.src = playlist.playlist_art;
 
     // creating title
-    const title = document.getElementByClassName("playlist-title");
-    title.textContent += "${playlist.playlist_name}";
+    const title = document.createElement("h2");
+    title.className = "playlist-title";
+    title.textContext = playlist.playlist_name;
 
     // creating creator tag
-    const creator = document.getElementByClassName("playlist-creator");
-    creator.textContent += "${playlist.playlist_creator}";
+    const creator = document.createElement("p");
+    creator.className = "playlist-creator";
+    creator.textContext = playlist.playlist_creator;
 
     // creating likes
-    const likes = document.getElementByClassName("likes");
-    likes.textContent += "${playlist.likeCount}";
+    const likes = document.createElement("div");
+    likes.className = "likes";
+
+    // Append elements to card
+    card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(creator);
+    card.appendChild(likes);
+    container.appendChild(card);
   }
 }
 
